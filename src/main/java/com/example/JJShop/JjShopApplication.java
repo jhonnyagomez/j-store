e0dc3ac3442ec3ad6cdede72fff979481fbef5d1
Javier Santiago <javiersantiago45011@gmail.com>
2024-04-03 22:42:54 -0500

    Base Project

---
+++ src/main/java/com/example/JJShop/JjShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.JJShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JJShop/JjShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JJShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
